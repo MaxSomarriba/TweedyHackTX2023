88f97db3b17e41235ef1daab545ddc1076059bd4
Max <maxmsomarriba@gmail.com>
2023-10-21 22:14:56 -0500

    fully working build

--- chrome-extension-v3-starter/popup/popup.js
+++ chrome-extension-v3-starter/popup/popup.js
@@ -17,7 +17,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     try {
         const url = 'http://127.0.0.1:5000/run_python_script';  // Replace with your server's URL
         const data = { data: array };
-        alert(array);
+        // alert(array);
         
 
         const response = await fetch(url, {
@@ -30,7 +30,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 
         if (response.ok) {
             const responseData = await response.json();
-            alert(responseData.result);
+            // alert(responseData.result);
             let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
@@ -54,60 +54,44 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 })
 
 function modifyPage(scores_array) {
-    // alert(scores_array);
-    // Get all elements with the attribute 'data-testid="tweetText"'
-    var tweetTextElements = document.querySelectorAll('[data-testid="tweetText"]');
+    const tweetTextElements = document.querySelectorAll('[data-testid="tweetText"]');
     var index = 0;
 
-    // Function to extract text from elements and differentiate by <span> elements
-    function extractTextWithSpan(element) {
-        if (element.nodeType === Node.ELEMENT_NODE) {
-            if (element.tagName === 'SPAN') {
-                if (scores_array[index] > 0.01) {
-                    element.style.color = 'red';
-                }
-                index++;
-
-                // element.style.color = 'red';
-            }
-            for (var i = 0; i < element.childNodes.length; i++) {
-                extractTextWithSpan(element.childNodes[i]);
-            }
-        }
-    }
-    //Iterate through elements with 'data-testid="tweetText"'
+    // Loop through the matching elements
     tweetTextElements.forEach(function (tweetTextElement) {
-        extractTextWithSpan(tweetTextElement);
+        if (scores_array[index] > 0.1) {
+            // tweetTextElement.style.color = "transparent";
+            // // tweetTextElement.style.textShadow = "0 0 10px rgba(0, 0, 0, .5)";
+            // tweetTextElement.style.backgroundClip = "text";
+            // tweetTextElement.style.webkitBackgroundClip = "text";
+            tweetTextElement.style.filter = "blur(5px)";
+            
+        }
     });
 }
 
 function scrapeFromPage() {
     // Get all elements with the attribute 'data-testid="tweetText"'
-    var tweetTextElements = document.querySelectorAll('[data-testid="tweetText"]');
+    // var tweetTextElements = document.querySelectorAll('[data-testid="tweetText"]');
 
     // Function to extract text from elements and differentiate by <span> elements
-    function extractTextWithSpan(element, textArray) {
-        if (element.nodeType === Node.ELEMENT_NODE) {
-            if (element.tagName === 'SPAN') {
-                textArray.push(element.textContent.trim());
+    // function extractTextWithSpan(element, textArray) {
+        // Get all elements with the attribute 'data-testid="tweetText"'
+        const tweetTextElements = document.querySelectorAll('[data-testid="tweetText"]');
 
-                // element.style.color = 'red';
-            }
-            for (var i = 0; i < element.childNodes.length; i++) {
-                extractTextWithSpan(element.childNodes[i], textArray);
-            }
-        }
+        // Initialize an array to store the inner text
+        const textArray = [];
+
+        // Loop through the matching elements
+        tweetTextElements.forEach(function (tweetTextElement) {
+            // Get the inner text content of each element
+            const innerText = tweetTextElement.textContent.trim();
+
+            // Add it to the array
+            textArray.push(innerText);
+        });
+        chrome.runtime.sendMessage({ textArray });
     }
-
-    // Initialize an array to store the extracted text
-    var textArray = [];
-
-    //Iterate through elements with 'data-testid="tweetText"'
-    tweetTextElements.forEach(function (tweetTextElement) {
-        extractTextWithSpan(tweetTextElement, textArray);
-    });
-    chrome.runtime.sendMessage({textArray});
-}
 
 // function onWindowLoad() {
 //     var message = document.getElementById('result');
